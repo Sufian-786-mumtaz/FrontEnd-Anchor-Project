66bc62e2ed2298cb73a755263ac81686d88244a0
sufianmumtaz <safyanmumtaz@gmail.com>
2023-01-09 18:41:20 +0500

    SET

--- middleware.ts
+++ middleware.ts
@@ -2,19 +2,19 @@ import {  NextRequest, NextResponse } from "next/server";
 export function middleware(req:NextRequest){    
     const token = req.cookies.get("session")
     
-    if(req.url.includes("http://localhost:3000/home") && !token){
-        return NextResponse.redirect("http://localhost:3000/login")
+    if(req.url.includes("https://front-end-final-project-xi.vercel.app/home") && !token){
+        return NextResponse.redirect("https://front-end-final-project-xi.vercel.app/login")
     }
-    if(req.url.includes("http://localhost:3000/cart") && !token){
-        return NextResponse.redirect("http://localhost:3000/login")
+    if(req.url.includes("https://front-end-final-project-xi.vercel.app/cart") && !token){
+        return NextResponse.redirect("https://front-end-final-project-xi.vercel.app/login")
     }
-    if(req.url.includes("http://localhost:3000/success") && !token){
-        return NextResponse.redirect("http://localhost:3000/login")
+    if(req.url.includes("https://front-end-final-project-xi.vercel.app/success") && !token){
+        return NextResponse.redirect("https://front-end-final-project-xi.vercel.app/login")
     }
-    if(req.url.includes("http://localhost:3000/login") && token){
-        return NextResponse.redirect("http://localhost:3000/home")
+    if(req.url.includes("https://front-end-final-project-xi.vercel.app/login") && token){
+        return NextResponse.redirect("https://front-end-final-project-xi.vercel.app/home")
     }
-    if(req.url.includes("http://localhost:3000/signup") && token){
-        return NextResponse.redirect("http://localhost:3000/home")
+    if(req.url.includes("https://front-end-final-project-xi.vercel.app/signup") && token){
+        return NextResponse.redirect("https://front-end-final-project-xi.vercel.app/home")
     }
 }
